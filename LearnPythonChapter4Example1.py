6bc2cd77a5687ac21a78442c72315ac1bec92522
Aniket Karmarkar <karmarkaraniket@gmail.com>
2025-12-15 16:06:41 -0600

    added example in book 1 chapter 4

---
+++ LearnPythonChapter4Example1.py
@@ -0,0 +1,12 @@
+def main():
+    a = 10
+    d = 10
+    b = "This program demonstrats Python Operators"
+    e = "This program demonstrats Python Operators"
+    c = [1,2,3]
+    f = [1,2,3]
+    print (a is not d)
+    print(b is e)
+    print(c is f)
+
+main()
