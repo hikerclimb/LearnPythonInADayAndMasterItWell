436e505590569ea237b68bce41fccf9ac4382748
Aniket Karmarkar <karmarkaraniket@gmail.com>
2025-12-15 16:11:52 -0600

    added example in chapter 4 excercise 2

---
+++ LearnPythonChapter4Example2.py
@@ -0,0 +1,11 @@
+def main():
+    x = 10
+    n = 12
+    print ("x > n is", x>n)
+    print ("x < n is", x<n)
+    print ("x == n is", x==n)
+    print ("x != n is", x!=n)
+    print ("x >= n is", x>=n)
+    print ("x <= n is", x<=n)
+
+main()
