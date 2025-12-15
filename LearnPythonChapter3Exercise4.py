cb2b07dee1ca7f68f33ed4706e337b512867ded0
Aniket Karmarkar <karmarkaraniket@gmail.com>
2025-12-15 15:54:58 -0600

    another way of doing problem

--- LearnPythonChapter3Exercise4.py
+++ LearnPythonChapter3Exercise4.py
@@ -2,6 +2,8 @@ def main():
     dictionary1 = {1:20, 4:40}
     dictionary2 = {6:60, 8:80}
     dictionary3 = {10:100, 12:120}
-    dictionaryfinal = dictionary1 | dictionary2 | dictionary3
+    dictionaryfinal = {}
+    for d in (dictionary1, dictionary2, dictionary3):
+        dictionaryfinal.update(d)
     print(dictionaryfinal)
 main()
