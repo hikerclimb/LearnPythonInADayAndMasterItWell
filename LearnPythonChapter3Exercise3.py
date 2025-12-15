f69179ac8b7b9b9364d5ae0cc5a22bb81ed8dbb4
Aniket Karmarkar <karmarkaraniket@gmail.com>
2025-12-15 15:20:38 -0600

    completed third excercise

---
+++ LearnPythonChapter3Exercise3.py
@@ -0,0 +1,8 @@
+def main():
+    lst = ['John', 'Peter', 'Sam', 'Tim', 'Janet', 'Dan']
+    lst.remove('John')
+    lst.remove('Sam')
+    lst.remove('Janet')
+    lst.remove('Dan')
+    print(lst)
+main()
