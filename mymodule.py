1eba3534568ab0d7f59dc074126a5e4e8439bc4c
Aniket Karmarkar <karmarkaraniket@gmail.com>
2025-12-16 11:46:20 -0600

    showed how to run in multiple files

---
+++ mymodule.py
@@ -0,0 +1,12 @@
+def main():
+    a = 10
+    d = 10
+    b = "This Program demonstrates Python operators."
+    e = "This Program demonstrates Python operators."
+    c = [1,2,3]
+    f = [1,2,3]
+    print(a is not d)
+    print(b is e)
+    print(c is f)
+
+main()
