3a6a89b1af5257f187a511b6cc7bba506138e2c0
Aniket Karmarkar <karmarkaraniket@gmail.com>
2025-12-18 16:48:22 -0600

    added examples from book

---
+++ Chapter8Example1.py
@@ -0,0 +1,6 @@
+def main(): 
+    pointer1 = 3
+    if (pointer1!=NULL):
+        free(pointer1)
+        pointer1 = NULL
+main()
---
+++ PythonCheatSheetList.py
@@ -0,0 +1,4 @@
+def main():
+    myList = ['virgo', 1, 'tauras', 'capricon', 3]
+    print(myList[2:-1])
+main()
